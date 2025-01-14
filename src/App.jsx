b0e2dc31dea43f3a27e9511c985c38c1ef78a0be
ore552 <osafule@gmail.com>
2025-01-14 19:02:32 +0000

    Everthinggit add . !

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './Pages/Homepage'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
     <>
       <div>
-
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Homepage/>}/>
+        </Routes>
       </div>
 
     </>
