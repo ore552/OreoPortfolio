853a0f02bc5d34ba064bd2f747516a5656605b8a
ore552 <osafule@gmail.com>
2025-02-13 18:16:29 +0000

    Nearly finished...

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        <p>Contact</p>
+    <div className='contact'>
+
+        <div className='row'>
+        <h2>Contact Me</h2>
+          <div className='col'>
+            <div className='col form'>
+              <form >
+                <p>Message me if you want to talk!</p>
+                <input type="text" placeholder='Name*' required/>
+                <input type="email"placeholder='Email*' required />
+                <textarea placeholder='Message*' required></textarea>
+                <button id='submit' type='submit'>Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+
     </div>
   )
 }
