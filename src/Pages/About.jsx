cfcbd85c94f4e032426c122fe779d1faa03b8952
ore552 <osafule@gmail.com>
2025-02-13 17:36:30 +0000

    Everything!

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-        <p>About</p>
+    <div className='aboutForm'>
+        <div className='contents'>
+          <div className='text lefts-column'>
+            <ul>
+              <h1>About Me</h1>
+            <li>
+              <h3>What I am</h3>
+              <p>I am Oreofe Olufirnaye, a curious front-end developer with an endless variety of innovative ideas to create pleasing, dynamic experiences.</p>
+            </li>
+            <li> 
+              <br />
+              <h3>What I do</h3> 
+              <p>I have a range of many skills some in coding, some in not: I have become an intermediate in HTML and CSS, using a variety of skills to create these specific projects; I have gotten a little bit in Javascript, with many different websites used in Javascipt (check out the Portfolio to see them all!) but I still need a little bit more work; I am currently learning and using React to make my websites and I am starting to get the hang of it - it is very different to what I used to do in HTML and CSS and JS! 
+              </p>
+              <li>
+                <h3>A description about me and my beliefs</h3>
+                Once I get into the project I am six feet in - and there is no getting out! I believe that once you get into something, you should finish it; I started writing books and none of them I have finished! I want to start starting and finsihing things. The only way you should actually stop a project (In my humble opinion) is when you know something is wrong and you don't know how to fix it so you start from scratch again or if you have gained new information and now you know that there is a lot of things you have to correct so you start again. 
+              <p></p>
+              </li>
+            </li>
+            <br />
+            </ul>
+          </div>
+
+          <div className='rights-column text'>
+
+          </div>
+        </div>
     </div>
   )
 }
